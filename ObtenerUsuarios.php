75b16a15129d83b304ec9c486fd1f94475d0ed28
roseroyeicok@gmail.com <roseroyeicok@gmail.com>
2020-06-19 15:38:46 -0500

    Vesrion 1.0

--- ObtenerUsuarios.php
+++ ObtenerUsuarios.php
@@ -1,5 +1,5 @@
 <?php 
-  header('Access-Control-Allow-Origin: http://localhost:4200'); 
+  header('Access-Control-Allow-Origin: *'); 
   header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
   
   require("conexion.php"); // IMPORTA EL ARCHIVO CON LA CONEXION A LA DB
