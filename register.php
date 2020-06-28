870be7368125e5e2bec6751145f8a1eeeac23d87
roseroyeicok@gmail.com <roseroyeicok@gmail.com>
2020-06-28 14:15:25 -0500

    Vesrion 1.0

--- register.php
+++ register.php
@@ -4,15 +4,15 @@ $postdata = file_get_contents("php://input");
 if(isset($postdata) && !empty($postdata))
 {
 $request = json_decode($postdata);
-$name = trim($request->nombre);
-$pwd = mysqli_real_escape_string($mysqli, trim($request->clave));
-$email = mysqli_real_escape_string($mysqli, trim($request->usuario));
+$nombre  = trim($request->nombre);
+$clave   = mysqli_real_escape_string($mysqli, trim($request->clave));
+$usuario = mysqli_real_escape_string($mysqli, trim($request->usuario));
 $sql = "INSERT INTO usuario(nombre,clave,usuario) VALUES ('$nombre','$clave','$usuario')";
 if ($mysqli->query($sql) === TRUE) {
 $authdata = [
-'nombre' => $name,
-'clave' => '',
-'usuario' => $email,
+'nombre' =>  $nombre,
+'clave' =>   $clave,
+'usuario' => $usuario,
 'idUsuario' => mysqli_insert_id($mysqli)
 ];
 echo json_encode($authdata);
