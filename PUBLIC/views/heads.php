15967a0a73fee30727973fce2007fba127703b32
yeicok rosero <roseroyeicok@gmail.com>
2020-09-29 12:08:44 -0500

    enfermeria verision 2.0

--- PUBLIC/views/heads.php
+++ PUBLIC/views/heads.php
@@ -28,10 +28,8 @@
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link type="text/css" href="/PUBLIC/css/sb-admin-2.min.css" />
-  <link type="text/css" href="/PUBLIC/css/dataTables.bootstrap.min.css" />
-  <link rel="stylesheet" href="/PUBLIC/estilos/forma.css" type="text/css" media="all"/>
-
-  
+  <link  href="/PUBLIC/css/sb-admin-2.min.css" rel="stylesheet" />
+  <link  href="/PUBLIC/css/dataTables.bootstrap.min.css" rel="stylesheet" />
+  <link  href="/PUBLIC/estilos/forma.css" rel="stylesheet" />
 
 </head>
